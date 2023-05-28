41afc49cae8e962445972db4cfda1446d3950c88
Noha Shehab <nohashehab.iti@gmail.com>
2023-05-28 12:41:24 +0300

    full chat app using python

--- charserver.py
+++ charserver.py
@@ -1,5 +1,6 @@
 from simple_websocket_server import WebSocketServer, WebSocket
 import  json
+# from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 
 
 def remove_dict_from_list(list_of_dict :list, key):
@@ -9,35 +10,45 @@ def remove_dict_from_list(list_of_dict :list, key):
 
     return  list_of_dict
 class Chatserver(WebSocket):
-    ## call some functions --> open connection to server, closing
     clients =[]
+    users = []
     def connected(self):
-        # print("==== this function will be called when client connected the server")
         print(self.address, 'connected')
-        # print(self.__class__)
-        # self.__class__.clients.append(self)
+        self.__class__.users.append(self)
 
     def handle_close(self):
-        # print("--- this function will be called when a client disconnected from the server ")
+        message= f"--->{self.username} has been disconnected\n"
         print(self.address, 'closed')
         self.__class__.clients = remove_dict_from_list(self.__class__.clients, self)
-        print(f"no of connected users till now {len(self.__class__.clients)}")
+        self.__class__.users.remove(self)
+        for user in self.__class__.users:
+                user.send_message(json.dumps({"message":message}))
 
 
 
 
 
     def handle(self):
-        # echo message back to client
-        print("=== this function will be called when the server receives message from the client ")
-        # self.send_message(self.data)
-        # print(f"message: {self.data}") # string --> hold json object
-        data =  json.loads(self.data)
-        print(data, type(data))
+        msg = self.prepare_message(self.data)
+        print(msg)
+        for user in self.__class__.users:
+            if user !=self:
+                user.send_message(msg)
+                print(f"--- message sent to client {user}")
+
+
+    def prepare_message(self, message):
+        data = json.loads(message)
+        print(data)
         if 'login' in data:
-            print("---hi")
-            self.__class__.clients.append({self:data['username']})
-            print(f"no of connected users till now {len(self.__class__.clients)}")
+            self.__class__.clients.append({self: data['username']})
+            self.username = data['username']
+            message_to_send = f"--->{data['username']} has been connected\n"
+        else:
+            message_to_send = f"{data['username']}:{data['message']}\n"
+
+        message_to_send = json.dumps({"message":message_to_send})
+        return  message_to_send
 
 
 
--- client.js
+++ client.js
@@ -1,25 +1,53 @@
 var username = window.prompt("Please enter your name: ")
 usernamediv= document.getElementById("username")
 usernamediv.innerText = username
-
-
-
-// use websocket to connect to the remote server
+let msg = document.getElementById('msg')
+let chat = document.getElementById('chat')
 
 let mywebsocket  = new WebSocket("ws://localhost:8000")
 console.log(mywebsocket)
 
 mywebsocket.onopen= function (){
-
-    // console.log("connection opened")
-    // send my to the server ?
     data_to_send = {
         "username": username,
         "login":true
     }
 
-    // sending data from client to server --> as string
     data = JSON.stringify(data_to_send)
-    // console.log(data, typeof data)
     mywebsocket.send(data)
 }
+
+
+mywebsocket.onmessage= function (event){
+    // console.log("--- message is being sent =====")
+    console.log(event.data, typeof data) // string
+    data = JSON.parse(event.data)
+    chat.innerHTML += data.message
+}
+
+mywebsocket.onerror = function (){
+    alert("server is not accepting connection right now ")
+}
+
+mywebsocket.onclose = function (){
+
+
+}
+
+msg.addEventListener("keyup",function (event) {
+    console.log(event)
+    if (event.code==="Enter"){
+        mymsg =msg.value
+        data_to_send = {
+        "message": mymsg,
+        "type":"chat",
+        "username":username
+        };
+
+        data= JSON.stringify(data_to_send);
+        mywebsocket.send(data);
+        msg.value='';
+        chat.innerHTML+="Me:"+mymsg+"\n"
+
+    }
+});
